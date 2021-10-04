853c4d077ffa3b84e2f6fb018f46c17d58d6ba0a
neoconstel <neoconstel@gmail.com>
2021-10-04 09:15:06 +0100

    added a test directory as well as a main.c in the test directory, from where we can make tests independent of ALX checking constraints

---
+++ test/main.c
@@ -0,0 +1,14 @@
+// This is where our tests will be carried out, thus the main() function will 
+// be placed here and not in _printf.c. This is because ALX will use their
+// automatic checker to place their own main file (containing a main() ) in the
+// root folder of the project.
+
+// Just note: ALX will not see this directory, so we can play around here. Only
+// the contents of the root directory matters to the automatic checker.
+
+
+
+
+int main(){
+
+}
