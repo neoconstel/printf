68553e662e3165d79261f60c06e25307e80ad23e
neoconstel <neoconstel@gmail.com>
2021-10-04 10:54:02 +0100

    added header_guard to main.h

--- main.h
+++ main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 
 int isin(char *string, const char c);
 
@@ -7,4 +8,6 @@ int count_valid_formatters(const char *string, ...);
 int printnum_as_chars(long n);
 
 int _printf(const char *string, ...);
+
+#endif
 
